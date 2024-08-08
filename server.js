72303588f6048dbb7a5c633a3609755de10ed269
Mangesh Dhok <dhokmangesh678@gmail.com>
2024-08-08 17:30:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const LeadModel= require('./LeadModel')
 const dotenv = require('dotenv')
 
 const app = express();
-
+const PORT = process.env.PORT || 5000;
 
 
 
@@ -84,7 +84,7 @@ app.delete('/deletebyid:id',  (req, res) => {
 });
 
 // Start Server
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
